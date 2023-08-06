997a6a41564873e44b3833b350a7e306f1780e3a
AlexandraHuber <alexandrahuber555@gmail.com>
2023-08-06 22:14:22 +0200

    added 3 trolls, changed tournament file, so it is working with new enemies

--- enemies.py
+++ enemies.py
@@ -6,7 +6,6 @@ from random import randint, choice
 class Enemy(Attacker):
     pass
 
-#FIXME ?
 def generate_random_enemy():
     RandomEnemyType = choice(enemy_types)
     enemy = RandomEnemyType()
@@ -17,6 +16,24 @@ def generate_dragon_list(enemy_number):
     enemy_list = [generate_random_enemy() for i in range(enemy_number)]
     return enemy_list
 
+def is_prime(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    return True
+
+def factor(n):
+    Ans = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            Ans.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        Ans.append(n)
+    return Ans
 
 class Dragon(Enemy):
     def set_answer(self, answer):
@@ -28,7 +45,7 @@ class Dragon(Enemy):
 
 class GreenDragon(Dragon): #addition
     def __init__(self):
-        self._health = 200
+        self._health = 100
         self._attack = 10
         self._color = 'зелёный'
 
@@ -39,12 +56,9 @@ class GreenDragon(Dragon): #addition
         self.set_answer(x + y)
         return self.__quest
 
-#FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
-# красный дракон учит вычитанию, а чёрный -- умножению - ok
-
 class RedDragon(Dragon): #substraction
     def __init__(self):
-        self._health = 250
+        self._health = 100
         self._attack = 10
         self._color = 'красный'
 
@@ -57,7 +71,7 @@ class RedDragon(Dragon): #substraction
 
 class BlackDragon(Dragon): #multiplication
     def __init__(self):
-        self._health = 150
+        self._health = 100
         self._attack = 10
         self._color = 'чёрный'
 
@@ -68,4 +82,84 @@ class BlackDragon(Dragon): #multiplication
         self.set_answer(x * y)
         return self.__quest
 
-enemy_types = [GreenDragon, RedDragon, BlackDragon]
+
+
+#FIXME Далее можете ввести новых атакующих юнитов:
+    """тролля, который задаёт вопрос "Угадай число от 1 до 5"
+    тролля, который задаёт вопрос на простоту числа
+    тролля, который просит разложить число на множители и перечислить их через запятую
+    """
+
+class GuessTroll(Dragon): #troll asks a hero to guess a number from 1 to 5
+    def __init__(self):
+        self._health = 100
+        self._attack = 10
+        self._color = 'quess_troll'
+
+    def question(self):
+        x = randint(1,4)
+        self.__quest = 'quess a number between 1 and 3'
+        self.set_answer(x)
+        return self.__quest
+
+class IfSimpleTroll(Dragon): #test for a simplisity of a number
+    def __init__(self):
+        self._health = 100
+        self._attack = 10
+        self._color = 'if_simple_troll'
+
+    def question(self):
+        x = randint(1,100)
+        self.__quest = f"Is {str(x)} a prime number? Answer in a format True/False"
+        self.set_answer(str(is_prime(x)))
+        return self.__quest
+
+class FactorsTroll(Dragon): #question for factors of a number
+    def __init__(self):
+        self._health = 100
+        self._attack = 10
+        self._color = 'factors_troll'
+
+    def question(self):
+        x = randint(1,11)
+        self.__quest = f"Say me all factors of {str(x)}"
+        self.set_answer(factor(x)) #FIXME
+        return self.__quest
+enemy_types = [GreenDragon, RedDragon, BlackDragon, GuessTroll, IfSimpleTroll, FactorsTroll]
+
+# def is_prime(n):
+#   for i in range(2,n):
+#     if (n%i) == 0:
+#       return print('No')
+#   return print('Yes')
+# print(type(is_prime(27)))
+
+def factor(n):
+    Ans = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            Ans.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        Ans.append(n)
+    return Ans
+
+print(factor(10))
+# print(" ".join(str(x) for x in factor(8)))
+
+# def input_factors_of_number(message):
+#     answer = None
+#     while answer == None:
+#         try:
+#             answer = []
+#             answer_0 = input(message).split(',')
+#             for i in range(len(answer_0)):
+#                 answer.append(int(answer_0[i]))
+#         except ValueError:
+#             print('Вы ввели недопустимые символы')
+#     return answer
+#
+# print(input_factors_of_number('message'))
--- tournament.py
+++ tournament.py
@@ -2,8 +2,23 @@
 # license: GPLv3
 from enemies import *
 from hero import *
+from tkinter import *
 
-def annoying_input_int(message =''):
+
+# root = Tk()
+# fr = Frame(root)
+# root.geometry('800x600')
+# canv = Canvas(root, bg='white')
+# canv.pack(fill=BOTH, expand=1)
+
+# self.id = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill=self.color)
+#
+#
+# def set_coords(self):
+#     canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+
+
+def annoying_input_int(message =''): #input of answers for dragons and quess_troll
     answer = None
     while answer == None:
         try:
@@ -12,23 +27,76 @@ def annoying_input_int(message =''):
             print('Вы ввели недопустимые символы')
     return answer
 
+def input_simple_number(message =''):
+    answer = None
+    while answer == None:
+        try:
+            answer = input(message)
+        except ValueError:
+            print('Вы ввели недопустимые символы')
+    return answer
+
+def input_factors_of_number(message =''):
+    answer = None
+    while answer == None:
+        try:
+            answer = []
+            answer_0 = input(message).split(',')
+            for i in range(len(answer_0)):
+                answer.append(int(answer_0[i]))
+        except ValueError:
+            print('Вы ввели недопустимые символы')
+    return answer
 
 def game_tournament(hero, dragon_list):
     for dragon in dragon_list:
-        print('Вышел', dragon._color, 'дракон!')
-        while dragon.is_alive() and hero.is_alive():
-            print('Вопрос:', dragon.question())
-            answer = annoying_input_int('Ответ:')
+        if type(dragon) == GreenDragon or type(dragon) == RedDragon or type(dragon) == BlackDragon or type(dragon) == GuessTroll:
+            print('Вышел', dragon._color, 'дракон!')
+            while dragon.is_alive() and hero.is_alive():
+                print('Вопрос:', dragon.question())
+                answer = annoying_input_int('Ответ:')
 
-            if dragon.check_answer(answer):
-                hero.attack(dragon)
-                print('Верно! \n** дракон кричит от боли **')
-            else:
-                dragon.attack(hero)
-                print('Ошибка! \n** вам нанесён удар... **')
-        if dragon.is_alive():
-            break
-        print('Дракон', dragon._color, 'повержен!\n')
+                if dragon.check_answer(answer):
+                    hero.attack(dragon)
+                    print('Верно! \n** дракон кричит от боли **')
+                else:
+                    dragon.attack(hero)
+                    print('Ошибка! \n** вам нанесён удар... **')
+            if dragon.is_alive():
+                break
+            print('Дракон', dragon._color, 'повержен!\n')
+
+        elif type(dragon) == IfSimpleTroll:
+            print('Вышел', dragon._color, 'дракон!')
+            while dragon.is_alive() and hero.is_alive():
+                print('Вопрос:', dragon.question())
+                answer = input_simple_number('Ответ:')
+
+                if dragon.check_answer(answer):
+                    hero.attack(dragon)
+                    print('Верно! \n** дракон кричит от боли **')
+                else:
+                    dragon.attack(hero)
+                    print('Ошибка! \n** вам нанесён удар... **')
+            if dragon.is_alive():
+                break
+            print('Дракон', dragon._color, 'повержен!\n')
+
+        elif type(dragon) == FactorsTroll:
+            print('Вышел', dragon._color, 'дракон!')
+            while dragon.is_alive() and hero.is_alive():
+                print('Вопрос:', dragon.question())
+                answer = input_factors_of_number('Ответ:')
+
+                if dragon.check_answer(answer):
+                    hero.attack(dragon)
+                    print('Верно! \n** дракон кричит от боли **')
+                else:
+                    dragon.attack(hero)
+                    print('Ошибка! \n** вам нанесён удар... **')
+            if dragon.is_alive():
+                break
+            print('Дракон', dragon._color, 'повержен!\n')
 
     if hero.is_alive():
         print('Поздравляем! Вы победили!')
@@ -43,9 +111,9 @@ def start_game():
         print('Представьтесь, пожалуйста: ', end = '')
         hero = Hero(input())
 
-        dragon_number = 3
+        dragon_number = 6
         dragon_list = generate_dragon_list(dragon_number)
-        assert(len(dragon_list) == 3)
+        assert(len(dragon_list) == 6)
         print('У Вас на пути', dragon_number, 'драконов!')
         game_tournament(hero, dragon_list)
 
