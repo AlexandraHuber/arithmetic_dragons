ecd4f439f7f89c1902bb5568a03c594454c87101
AlexandraHuber <alexandrahuber555@gmail.com>
2023-08-11 20:24:19 +0200

    Fixed buttons with functions

--- button_game.py
+++ button_game.py
@@ -7,29 +7,21 @@ class ButtonGame:
         self.root.title('Button game')
         self.root.geometry("200x100")
 
-        self.get_button = Button(text="Button 1", command=self.root.destroy)
+        self.get_button = Button(text="Button 1", command=self.create_button2)
+        self.get_button.pack(pady=20)
+
+    def create_button2(self):
+        self.get_button.destroy()
+        self.get_button = Button(text="Button 2", command=self.create_button3)
+        self.get_button.pack(pady=20)
+
+    def create_button3(self):
+        self.get_button.destroy()
+        self.get_button = Button(text="Button 3", command=self.root.destroy)
         self.get_button.pack(pady=20)
 
     def start_game(self):
         self.root.mainloop()
-
-        root = Tk()
-        root.title('Button game')
-        root.geometry("200x100")
-
-        get_button = Button(text="Button 2", command=root.destroy)
-        get_button.pack(pady=20)
-
-        root.mainloop()
-
-        root = Tk()
-        root.title('Button game')
-        root.geometry("200x100")
-
-        get_button = Button(text="Button 3", command=root.destroy)
-        get_button.pack(pady=20)
-
-        root.mainloop()
 
 
 if __name__ == '__main__':
