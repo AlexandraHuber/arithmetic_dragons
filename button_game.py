279b3bc7ecd0709f98eb4f6d315d8e92faf8745a
AlexandraHuber <alexandrahuber555@gmail.com>
2023-08-11 20:02:16 +0200

    Moved button 1 initialisation in the class

--- button_game.py
+++ button_game.py
@@ -2,19 +2,19 @@ from tkinter import *
 
 
 class ButtonGame:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title('Button game')
+        self.root.geometry("200x100")
+
+        self.get_button = Button(text="Button 1", command=self.root.destroy)
+        self.get_button.pack(pady=20)
 
     def start_game(self):
-        root = Tk()
-        root.title('Dragon game')
-        root.geometry("200x100")
-
-        get_button = Button(text="Button 1", command=root.destroy)
-        get_button.pack(pady=20)
-
-        root.mainloop()
+        self.root.mainloop()
 
         root = Tk()
-        root.title('Dragon game')
+        root.title('Button game')
         root.geometry("200x100")
 
         get_button = Button(text="Button 2", command=root.destroy)
@@ -23,7 +23,7 @@ class ButtonGame:
         root.mainloop()
 
         root = Tk()
-        root.title('Dragon game')
+        root.title('Button game')
         root.geometry("200x100")
 
         get_button = Button(text="Button 3", command=root.destroy)
